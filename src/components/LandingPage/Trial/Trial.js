b7fec1ff5e35263596dcc63de7ad7940eaedabda
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-12-09 17:10:45 -0500

    finished trial section

--- src/components/LandingPage/Trial/Trial.js
+++ src/components/LandingPage/Trial/Trial.js
@@ -1,40 +1,34 @@
+// Trial.js
 import React from 'react';
-import { Box, Typography } from '@mui/material';
-import MiniBarBubble from "../../MiniBarBubble/MiniBarBubble";
+import { Container, Typography } from '@mui/material';
+import './Trial.css';
+import {Link as RouterLink} from "react-router-dom"; // Add your styling if needed
 
 const Trial = () => {
     return (
-        <Box sx={{ textAlign: 'center', padding: '40px', my: 20 }}>
-            {/* FAQ Title */}
-            <MiniBarBubble
-
-                backgroundColor="rgba(96, 2, 124, 0.15)"
-                color="#60027C"
-            >
-                <Typography variant={"body1"} color={"#60027C"}>
-                    FAQ
-                </Typography>
-                <Typography variant={"body1"} color={"rgba(96, 2, 124, 0.5)"}>
-                    Help Center
-                </Typography>
-            </MiniBarBubble>
-
-
-            {/* Have a question? */}
-            <Typography variant="h2" sx={{ my: 4 }}>
-                Have a question?
-            </Typography>
-
-            {/* Support Team */}
-            <Typography variant="body1" sx={{ mb: 4 }}>
-                Our support team will get assistance from AI-powered suggestions, making it quicker than ever to handle support requests.
-            </Typography>
-
-            {/* Placeholder for the last element */}
-            <Box>
-                {/* Add your content for the last element here */}
-            </Box>
-        </Box>
+        <Container maxWidth="md" className="trial-container" sx={{ py: 10 }}>
+            <div className="main-card-outer">
+                <div className={"main-card-inner"}>
+                    <div className="logo-row">
+                        <div className="logo-bubble">Unlock AI</div>
+                        <div className="logo-text">UNLOCK YOUR CREATIVITY</div>
+                    </div>
+                    <Typography mb="1rem" variant="h1" className="trial-title">
+                        Start Your<br/>Free Trial
+                    </Typography>
+                    <Typography mb="1rem" variant="body1" className="description" sx={{maxWidth: "65%", mx: "auto"}}>
+                        Unlock your business potential by letting the AI work and generate money for you.
+                    </Typography>
+                    <Typography
+                        component={RouterLink}
+                        to="/signup"
+                        className="trial-button"
+                    >
+                        Join Our Community
+                    </Typography>
+                </div>
+            </div>
+        </Container>
     );
 };
 
