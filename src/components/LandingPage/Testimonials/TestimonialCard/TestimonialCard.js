ca86befc5c916b3ba21bf8eeaed9835b911577bf
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-12-09 17:10:11 -0500

    improved testimonial and card styling

--- src/components/LandingPage/Testimonials/TestimonialCard/TestimonialCard.js
+++ src/components/LandingPage/Testimonials/TestimonialCard/TestimonialCard.js
@@ -1,30 +1,38 @@
+// TestimonialCard.js
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
-import './TestimonialCard.css';  // Create a CSS file for styling if needed
+import './TestimonialCard.css';
 
 const TestimonialCard = ({ profile, author, testimonial }) => {
+    const placeholderImage = 'https://source.unsplash.com/150x150/?portrait';
+
     return (
         <div className="testimonial-card">
-            <div style={{ display: "flex", flexDirection: "row" }}>
-                <img style={{ flex: 1 }} src={profile} alt="Profile" className="profile-image" />
-                <Typography sx={{ flex: 1, display: 'flex', alignItems: 'center', fontSize: "1.1rem", fontWeight: 800 }} variant="body1">{author}</Typography>
+            <div className="top-row">
+                <img
+                    src={profile || placeholderImage}
+                    alt="Profile"
+                    className="profile-image"
+                />
+                <Typography variant="body1" className="author">{author}</Typography>
             </div>
-            <Typography fontSize={"1rem"} mt={2} textAlign={"center"} variant="body2" color={"rgba(0,0,0,0.5)"}>{testimonial}</Typography>
+            <Typography variant="body2" className="testimonial-text">{testimonial}</Typography>
         </div>
     );
 };
 
 TestimonialCard.propTypes = {
-    profile:     PropTypes.string,
-    author:        PropTypes.string,
+    profile: PropTypes.string,
+    author: PropTypes.string,
     testimonial: PropTypes.string,
 };
 
 TestimonialCard.defaultProps = {
-    profile: 'default-image.jpg',
+    profile: '',
     author: 'Social media Influencer',
-    testimonial:'This tool saved me so much time and effort. It helped me produce content that was more engaging and effective.',
+    testimonial: 'This tool saved me so much time and effort. It helped me produce content that was more engaging and effective.',
 };
 
 export default TestimonialCard;
--- src/components/LandingPage/Testimonials/Testimonials.js
+++ src/components/LandingPage/Testimonials/Testimonials.js
@@ -1,13 +1,40 @@
 // Testimonials.js
 
 import React from 'react';
-import {Box, Container, Grid, Typography} from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import TestimonialCard from './TestimonialCard/TestimonialCard';
 import MiniBarBubble from "../../MiniBarBubble/MiniBarBubble";
 
+const testimonialsData = [
+    {
+        author: "John Doe",
+        testimonial: "This tool transformed the way I create content. It's a game-changer!"
+    },
+    {
+        author: "Jane Doe",
+        testimonial: "I've never seen such innovative features. It's a must-have for content creators!"
+    },
+    {
+        author: "Alex Smith",
+        testimonial: "UnlockAI has saved me time and effort. The AI-generated content is incredible! I can't imagine my content creation process without it."
+    },
+    {
+        author: "Emily Johnson",
+        testimonial: "As a social media influencer, UnlockAI has enhanced my content strategy significantly. The ability to generate creative captions and hashtags is a game-changer."
+    },
+    {
+        author: "Michael Brown",
+        testimonial: "The AI tools provided by UnlockAI are simply amazing. Highly recommended!"
+    },
+    {
+        author: "Sophia Williams",
+        testimonial: "Joining the UnlockAI community was the best decision for my content creation journey. The support from the community and the innovative AI features have made a positive impact on my work."
+    },
+];
+
 const Testimonials = ({ id }) => {
     return (
-        <Box id={id} sx={{ textAlign: 'center', py: 20 }}>
+        <Box id={id} sx={{ textAlign: 'center', py: 10 }}>
             {/* Header */}
             <MiniBarBubble color={"rgba(40, 2, 124, 0.15)"} backgroundColor={"rgba(40, 2, 124, 0.15)"}>
                 <Typography variant={"body1"} color={"#28027C"}>
@@ -24,48 +51,15 @@ const Testimonials = ({ id }) => {
 
             <Container maxWidth={"lg"}>
                 {/* Testimonials Grid */}
-                <Grid container spacing={3} justifyContent="center"> {/* Center the grid items */}
-                    {/* Testimonial 1 */}
-                    <Grid item xs={12} sm={6} md={4}>
-                        <TestimonialCard
-                            author="John Doe"
-                        />
-                    </Grid>
-
-                    {/* Testimonial 2 */}
-                    <Grid item xs={12} sm={6} md={4}>
-                        <TestimonialCard
-                            author="Jane Doe"
-                        />
-                    </Grid>
-
-                    {/* Testimonial 3 */}
-                    <Grid item xs={12} sm={6} md={4}>
-                        <TestimonialCard
-                            author="Alex Smith"
-                        />
-                    </Grid>
-
-                    {/* Testimonial 4 */}
-                    <Grid item xs={12} sm={6} md={4}>
-                        <TestimonialCard
-                            author="Emily Johnson"
-                        />
-                    </Grid>
-
-                    {/* Testimonial 5 */}
-                    <Grid item xs={12} sm={6} md={4}>
-                        <TestimonialCard
-                            author="Michael Brown"
-                        />
-                    </Grid>
-
-                    {/* Testimonial 6 */}
-                    <Grid item xs={12} sm={6} md={4}>
-                        <TestimonialCard
-                            author="Sophia Williams"
-                        />
-                    </Grid>
+                <Grid container spacing={3} justifyContent="center">
+                    {testimonialsData.map((testimonial, index) => (
+                        <Grid key={index} item xs={12} sm={6} md={4}>
+                            <TestimonialCard
+                                author={testimonial.author}
+                                testimonial={testimonial.testimonial}
+                            />
+                        </Grid>
+                    ))}
                 </Grid>
             </Container>
         </Box>
